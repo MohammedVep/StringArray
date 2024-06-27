16e62807e91764b2804174421b5ef9b8d4d7e96c
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2024-06-27 16:12:27 -0400

    Completed Initialize and print string array

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Grapes", "Orange", "Bannana"};
+        System.out.println("Fruits inside the array is listed below: ");
+        System.out.println(fruits[0]);
+        System.out.println(fruits[1]);
+        System.out.println(fruits[2]);
+        System.out.println(fruits[3]);
+    }
+}
